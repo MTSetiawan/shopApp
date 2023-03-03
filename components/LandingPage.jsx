b2626c327afc45279b26b6d822d4b455d11e96a3
MTSetiawan <mt.dev022@gmail.com>
2023-03-03 21:09:57 +0700

    create navbar and carousel

---
+++ components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./utils/navbar";
+import Carousel from "./utils/carousel";
+
+export default function Landingpage() {
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+    </>
+  );
+}
